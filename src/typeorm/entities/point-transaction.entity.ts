905bfed0e6a0cd667453aadf01204a03a9b35d3a
whitekiwi <jh145478@gmail.com>
2021-08-25 16:36:00 +0900

    fix(PointTransaction): 누락된 userId 추가

--- src/typeorm/entities/point-transaction.entity.ts
+++ src/typeorm/entities/point-transaction.entity.ts
@@ -12,7 +12,7 @@ export enum PointTransactionEventTypeEnum {
 export type PointTransactionEventType =
 	keyof typeof PointTransactionEventTypeEnum
 
-@Index('source_id_event_type_index', ['sourceId', 'eventType'])
+@Index('source_id_event_type_index', ['resourceId', 'eventType'])
 @Entity({ name: 'point_transactions' })
 export class PointTransaction {
 	@PrimaryGeneratedColumn('uuid')
@@ -21,8 +21,12 @@ export class PointTransaction {
 	@Column('enum', { enum: PointTransactionEventTypeEnum })
 	eventType!: PointTransactionEventType
 
-	@Column('uuid', { name: 'source_id' })
-	sourceId!: string
+	@Column('uuid', { name: 'resource_id' })
+	resourceId!: string
+
+	@Index('user_id_index')
+	@Column('uuid', { name: 'user_id' })
+	userId!: string
 
 	@Column('int', { unsigned: true })
 	amount!: number
